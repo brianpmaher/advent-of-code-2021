f93d6b3cb356f7a885c276b4660abb4e4c634d8d
Brian Maher <bmaher91@gmail.com>
2021-12-06 20:23:15 -0700

    day 6 part 2 done

--- day-6/day-6.js
+++ day-6/day-6.js
@@ -5,7 +5,7 @@ const lanternfishLifeTimers = fs
  .split(',')
  .map(numChar => parseInt(numChar));
 
-function updateLanternfishLifeTimers() {
+function updateLanternfishLifeTimers(lanternfishLifeTimers) {
  const lanternFishTimersLength = lanternfishLifeTimers.length
 
  for (let i = 0; i < lanternFishTimersLength; i++) {
@@ -17,10 +17,49 @@ function updateLanternfishLifeTimers() {
 
    lanternfishLifeTimers[i]--;
  }
+
+ console.log('length', lanternfishLifeTimers.length);
 }
 
-for (let day = 0; day < 80; day++) {
- updateLanternfishLifeTimers();
+function part1() {
+  const timersClone = [...lanternfishLifeTimers];
+
+  for (let day = 0; day < 80; day++) {
+    updateLanternfishLifeTimers(timersClone);
+  }
+
+  return timersClone.length;
 }
 
-console.log(lanternfishLifeTimers.length);
+function part2() {
+  const numTimersByNumber = {
+    0: 0,
+    1: 0,
+    2: 0,
+    3: 0,
+    4: 0,
+    5: 0,
+    6: 0,
+    7: 0,
+    8: 0
+  };
+
+  lanternfishLifeTimers.forEach((value) => {
+    numTimersByNumber[value]++;
+  });
+
+  for (let days = 0; days < 256; days++) {
+    const numZeros = numTimersByNumber[0];
+
+    for (let i = 1; i < 9; i++) {
+      numTimersByNumber[i-1] = numTimersByNumber[i];
+    }
+
+    numTimersByNumber[6] += numZeros;
+    numTimersByNumber[8] = numZeros;
+  }
+
+  return Object.values(numTimersByNumber).reduce((sum, val) => sum + val, 0);
+}
+
+console.log(part2());
