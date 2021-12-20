5767804701a5ac7617d03911072d3ea1cfb0e3fe
Brian Maher <bmaher91@gmail.com>
2021-12-19 19:32:39 -0700

    day 14 part 2 done

--- day-14/day-14.js
+++ day-14/day-14.js
@@ -4,37 +4,47 @@ const rawInput = fs.readFileSync('input.txt', 'utf8');
 
 let [template, ...pairInsertions] = rawInput.split('\r\n').filter(line => line.length);
 
-const pairMap = pairInsertions
+console.log('template', template);
+
+let pairMap = pairInsertions
     .map(insertion => insertion.split(' -> '))
-    .reduce((map, [pair, insertion]) => ({
+    .reduce((map, [pair, outLetter]) => ({
         ...map,
-        [pair]: `${pair[0]}${insertion}`,
+        [pair]: outLetter
     }), {});
 
-console.log('\n');
-console.log(`Template\t${template}`);
-for (let stepCount = 0; stepCount < 10; stepCount++) {
-    let newTemplate = '';
-
-    for (let i = 0; i < template.length - 1; i++) {
-        const pair = template[i] + template[i + 1];
-        const outPair = pairMap[pair];
-        newTemplate += outPair;
-    }
-
-    newTemplate += template[template.length - 1];
-
-    template = newTemplate;
-    console.log(`After step ${stepCount + 1}:\t${template.length}`);
+// Taking the initial template and setting the pair counts
+let pairCounts = {};
+for (let i = 0; i < template.length - 1; i++) {
+    const pair = template[i].concat(template[i+1]);
+    pairCounts[pair] = (pairCounts[pair] || 0) + 1;
 }
 
-const elementCounts = template.split('').reduce((elementCounts , element) => ({
-    ...elementCounts,
-    [element]: (elementCounts[element] || 0) + 1
+// Taking the initial template and setting the letter counts
+const letterCounts = template.split('').reduce((letterCounts, letter) => ({
+    ...letterCounts,
+    [letter]: (letterCounts[letter] || 0) + 1
 }), {});
 
-const mostCommon  = Math.max(...Object.values(elementCounts));
-const leastCommon = Math.min(...Object.values(elementCounts));
+for (let stepCount = 0; stepCount < 40; stepCount++) {
+    const newPairCounts = {};
+    Object.keys(pairCounts).forEach((pair) => {
+        const letter = pairMap[pair];
+        const pair1 = pair[0].concat(letter);
+        const pair2 = letter.concat(pair[1]);
+        newPairCounts[pair1] = (newPairCounts[pair1] || 0) + pairCounts[pair];
+        newPairCounts[pair2] = (newPairCounts[pair2] || 0) + pairCounts[pair];
+        letterCounts[letter] = (letterCounts[letter] || 0) + pairCounts[pair];
+    });
+    pairCounts = newPairCounts;
+}
 
-const result = mostCommon - leastCommon;
-console.log('result', result);
+console.log(pairMap);
+console.log(letterCounts);
+console.log(pairCounts);
+console.log(Object.values(letterCounts).reduce((sum, v) => sum + v, 0));
+const largest = Math.max(...Object.values(letterCounts));
+console.log('largest', largest);
+const smallest = Math.min(...Object.values(letterCounts));
+console.log('smallest', smallest);
+console.log('result', largest - smallest);
